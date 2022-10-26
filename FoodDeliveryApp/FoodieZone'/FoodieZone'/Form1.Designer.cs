850752b1d0abf62e8beced9c399e6a7c4b89f17c
shreyab1904 <84987805+shreyab1904@users.noreply.github.com>
2022-10-26 11:32:14 +0530

    Add files via upload

---
+++ FoodDeliveryApp/FoodieZone'/FoodieZone'/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace FoodieZone_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.button_register = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(203, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(352, 114);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(209, 27);
+            this.username.TabIndex = 5;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(352, 158);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(209, 27);
+            this.email.TabIndex = 6;
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(352, 200);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(209, 27);
+            this.phone.TabIndex = 7;
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(352, 241);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(209, 27);
+            this.address.TabIndex = 8;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(352, 285);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(209, 27);
+            this.password.TabIndex = 9;
+            // 
+            // button_register
+            // 
+            this.button_register.Location = new System.Drawing.Point(326, 349);
+            this.button_register.Name = "button_register";
+            this.button_register.Size = new System.Drawing.Size(127, 29);
+            this.button_register.TabIndex = 10;
+            this.button_register.Text = "Register";
+            this.button_register.UseVisualStyleBackColor = true;
+            this.button_register.Click += new System.EventHandler(this.button_register_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(336, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 38);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Welcome";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.BackgroundImage = global::FoodieZone_.Properties.Resources.yellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button_register);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button button_register;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ FoodDeliveryApp/FoodieZone'/FoodieZone'/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FoodieZone_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_register_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=INPHWN123679;Initial Catalog=Fooddelivery;Integrated Security=True");
+         
+            SqlCommand cmd = new SqlCommand(@" INSERT INTO [dbo].[user_register]
+           ([User_name]
+           ,[User_email]
+           ,[User_phone]
+           ,[User_address]
+           ,[User_password])
+     VALUES
+           ('"+username.Text+"','" + email.Text + "','" + phone.Text + "','" + address.Text + "','" + password.Text + "')", conn);
+
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registeration Successfull!!");
+
+        }
+    }
+}
