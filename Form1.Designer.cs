f57dc0e5577abd15fcb5c2897c52a46dee7aa1f2
RishabhHarman04 <101667621+RishabhHarman04@users.noreply.github.com>
2022-10-28 16:55:34 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace RestaurantRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.address = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.emailid = new System.Windows.Forms.TextBox();
+            this.mobileno = new System.Windows.Forms.TextBox();
+            this.ownername = new System.Windows.Forms.TextBox();
+            this.restaurantname = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labe2 = new System.Windows.Forms.Label();
+            this.labe1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImage = global::RestaurantRegistration.Properties.Resources.yellow1;
+            this.groupBox1.Controls.Add(this.address);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.emailid);
+            this.groupBox1.Controls.Add(this.mobileno);
+            this.groupBox1.Controls.Add(this.ownername);
+            this.groupBox1.Controls.Add(this.restaurantname);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.labe2);
+            this.groupBox1.Controls.Add(this.labe1);
+            this.groupBox1.Location = new System.Drawing.Point(54, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(984, 607);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RegistrationPage";
+            // 
+            // address
+            // 
+            this.address.Location = new System.Drawing.Point(352, 274);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(517, 26);
+            this.address.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 37);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "address";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.OrangeRed;
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(352, 550);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(565, 57);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Already have an account? Login";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(352, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(565, 57);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Regiter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(352, 407);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(517, 26);
+            this.password.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Email-Id";
+            // 
+            // emailid
+            // 
+            this.emailid.Location = new System.Drawing.Point(352, 341);
+            this.emailid.Name = "emailid";
+            this.emailid.Size = new System.Drawing.Size(517, 26);
+            this.emailid.TabIndex = 8;
+            // 
+            // mobileno
+            // 
+            this.mobileno.Location = new System.Drawing.Point(352, 214);
+            this.mobileno.Name = "mobileno";
+            this.mobileno.Size = new System.Drawing.Size(517, 26);
+            this.mobileno.TabIndex = 7;
+            // 
+            // ownername
+            // 
+            this.ownername.Location = new System.Drawing.Point(352, 131);
+            this.ownername.Name = "ownername";
+            this.ownername.Size = new System.Drawing.Size(517, 26);
+            this.ownername.TabIndex = 6;
+            // 
+            // restaurantname
+            // 
+            this.restaurantname.Location = new System.Drawing.Point(352, 62);
+            this.restaurantname.Name = "restaurantname";
+            this.restaurantname.Size = new System.Drawing.Size(517, 26);
+            this.restaurantname.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(61, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 37);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 413);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 37);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mobile";
+            // 
+            // labe2
+            // 
+            this.labe2.AutoSize = true;
+            this.labe2.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labe2.Location = new System.Drawing.Point(40, 137);
+            this.labe2.Name = "labe2";
+            this.labe2.Size = new System.Drawing.Size(166, 37);
+            this.labe2.TabIndex = 1;
+            this.labe2.Text = "Owner Name";
+            // 
+            // labe1
+            // 
+            this.labe1.AutoSize = true;
+            this.labe1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labe1.Location = new System.Drawing.Point(40, 69);
+            this.labe1.Name = "labe1";
+            this.labe1.Size = new System.Drawing.Size(223, 37);
+            this.labe1.TabIndex = 0;
+            this.labe1.Text = "Restaurant Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(266, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(503, 52);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Restaurant Registration";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RestaurantRegistration.Properties.Resources.yellow;
+            this.ClientSize = new System.Drawing.Size(1050, 687);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox emailid;
+        private System.Windows.Forms.TextBox mobileno;
+        private System.Windows.Forms.TextBox ownername;
+        private System.Windows.Forms.TextBox restaurantname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labe2;
+        private System.Windows.Forms.Label labe1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox address;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestaurantRegistration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=INBAWN170255\\SQLEXPRESS;Initial Catalog=Fooddelivery;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@" INSERT INTO [dbo].[RestaurantRegistrationInfo]
+           ([Restaurant Name]
+           ,[Owner Name]
+           ,[Mobile No]
+           ,[Email-Id]
+           ,[Address]
+           ,[Password])
+ 
+     VALUES
+             ('" + restaurantname.Text + "','" + ownername.Text + ",'" +mobileno.Text + "','"+emailid.Text+"','" + address.Text+ "', '"+password.Text +"' )", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registeration Successfull!!");
+        }
+    }
+}
